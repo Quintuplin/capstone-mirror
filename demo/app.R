a2d2714f5a89815f85634cd6a3ac5ade92356fbd
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-02-18 16:31:05 -0500

    implemented base level support for core functionality
    has file input, call to msconvert
    
    todo still:
    drag and drop
    actual file conversion
    converted file save

--- demo/app.R
+++ demo/app.R
@@ -1,13 +1,16 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# #
+# # This is a Shiny web application. You can run the application by clicking
+# # the 'Run App' button above.
+# #
+# # Find out more about building applications with Shiny here:
+# #
+# #    http://shiny.rstudio.com/
+# #
+
+# library(shiny)
+
+# # Run the application 
+# shinyApp(ui = ui.R, server = Server.R)
 
 library(shiny)
-
-# Run the application 
-shinyApp(ui = ui.R, server = Server.R)
+library(mzR)
--- demo/ui.R
+++ demo/ui.R
@@ -1,22 +1,60 @@
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+#   # Application title
+#   titlePanel("Old Faithful Geyser Data"),
   
-  # Sidebar with a slider input for number of bins 
+#   # Sidebar with a slider input for number of bins 
+#   sidebarLayout(
+#     sidebarPanel(
+#       sliderInput("bins",
+#                   "Number of bins:",
+#                   min = 1,
+#                   max = 50,
+#                   value = 30)
+#     ),
+    
+#     # Show a plot of the generated distribution
+#     mainPanel(
+#       plotOutput("distPlot")
+#     )
+#   )
+# )
+
+  titlePanel("Drag-and-drop RAW to mzML converter"),
+  
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      fileInput("rawFile", "Upload Thermo raw file",
+                multiple = FALSE,
+                accept = c(".raw")),
+
+      # checkboxInput("AutoInfer", "AutoInfer parameters", TRUE),
+      # txt = "My reactive title",
+      # radioButtons("sep", "Standard",
+      #              choices = c(HeLa = "HeLa",
+      #                          UPS1 = "UPS1",
+      #                          UPS2 = "UPS2",
+      #                          Neeley = "Neeley"),
+      #              selected = "HeLa"),
+
+      # radioButtons('SpectaST', 'SpectraST version',
+      #              choices = c('4.0' = '4',
+      #                          '5.0' = '5'),
+      #              selected = '5'),
+     
+      # radioButtons("depth", "QC depth",
+      #              choices = c(Quick = 'Quick',
+      #                          Full = 'Full'),
+      #              selected = 'Quick'),
+  
+      actionButton("QC", "Run Converter")
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      textOutput('text'),
+      plotOutput('TICplot'),
+      plotOutput('PSMplot')
     )
   )
 )
