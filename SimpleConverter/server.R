eb6a8f90a3f9f4eb11b8d675492fed15e67dc632
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-02-27 13:39:59 -0500

    uploaded thermoraw, minor updates to simpleconverter
    simpleconverter needs more changes to actually correctly call thermoraw. thermoraw has an install process (see notes.txt), and has a specific call method that I am figuring out
    
    WIP commit

--- SimpleConverter/server.R
+++ SimpleConverter/server.R
@@ -3,6 +3,6 @@ options(shiny.maxRequestSize=5*1024^3)   # 5 GB maximum file size...
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   observeEvent(input$QC, {
-    file.copy(system(paste('msconvert ', input$rawFile$datapath)), 'data.mxML', overwrite = TRUE)
+    file.copy(system(paste('T', input$rawFile$datapath)), "~/Desktop/folderfolder/x.mxML", overwrite = TRUE)
   })
 }
