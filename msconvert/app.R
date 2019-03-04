44668e8906e8d97c24b5b042df409368700f59a8
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-03-04 13:53:03 -0500

    made it work
    ms convert works on my machine
    
    install will be necessary for it to work on the VM
    
    issue closed

--- msconvert/app.R
+++ msconvert/app.R
@@ -3,10 +3,12 @@ library(mzR)
 
 options(shiny.maxRequestSize=5*1024^3)   # 5 GB maximum file size...
 # setwd('C:/Users/nmpalmblad/Documents/Projects/Ben/')
+my_output_dir <- ('C:/Users/quint/OneDrive/Desktop/folderfolder')
 
 ui <- fluidPage(
-
+  
   titlePanel("msconvert"),
+  
   
   sidebarLayout(
     sidebarPanel(
@@ -26,8 +28,9 @@ ui <- fluidPage(
 server <- function(input, output) {
 
   observeEvent(input$QC, {
-    file.copy(system(paste('msconvert ', input$rawFile$datapath, ' -v --mzML', sep='')), "~/Desktop/folderfolder/x.txt" ,overwrite = TRUE)
-   
+    #file.copy(system(paste('msconvert ', input$rawFile$datapath, ' -v --mzML', sep='')), "~/Desktop/folderfolder/x.txt" ,overwrite = TRUE)
+    system(paste('C:/APPWin32bitBundle/APPServer/bin/pwiz/msconvert.exe ', input$rawFile$datapath, ' -o ', my_output_dir), intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE, wait = TRUE, show.output.on.console = TRUE, minimized = FALSE, invisible = FALSE)
+    system('echo Done')
     }
   )}
 
