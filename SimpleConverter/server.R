d883b3694edbad1db8604519789b18215d63889f
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-03-06 10:29:05 -0500

    clweN

--- SimpleConverter/server.R
+++
@@ -1,8 +0,0 @@
-options(shiny.maxRequestSize=5*1024^3)   # 5 GB maximum file size...
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  observeEvent(input$QC, {
-    file.copy(system(paste('T', input$rawFile$datapath)), "~/Desktop/folderfolder/x.mxML", overwrite = TRUE)
-  })
-}
