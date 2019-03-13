8a58fa32a889dc6ff7e82696f12255386bb8f75b
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-03-13 11:10:23 -0400

    merged from dylancode the old-fasioned way
    avoiding further github shenanigans

---
+++ dash/Dylancode/shiny-old/ui.R
@@ -0,0 +1,54 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+# NIST CofC pipeline page
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Results"),
+  
+  # Sidebar just says "Hela" for now
+  sidebarLayout(
+    sidebarPanel("Hela"),
+    
+    # Show a plot of the generated distribution
+    #mainPanel(
+    #  tabsetPanel(
+    #    tabPanel("Plot",plotOutput("distPlot")),
+    #    tabPanel("Summary",verbatimTextOutput("summary"))
+    #  ))
+    fluidRow(column(12, offset = 0, "Pipeline",
+                    fluidRow(column(2,offset= 1,"Sample"),
+                             column(2,"LC"),
+                             column(2,"Source"),
+                             column(2,"MS-1"),
+                             column(2,"MS-2")
+                             ),
+                    #Hard Coded colored squares for now
+                    #
+                    #arrow.png needs all columns set to 1 and may not be necessary
+                    fluidRow(column(2,offset= 1,img(src="images/greenbox.png",width=100)),
+                                #column(1,img(src="images/arrow.png",width=50)),
+                             column(2,img(src="images/greenbox.png",width=100)),
+                                #column(1,img(src="images/arrow.png",width=50)),
+                             column(2,img(src="images/yellowbox.png",width=100)),
+                                #column(1,img(src="images/arrow.png",width=50)),
+                             column(2,img(src="images/redbox.png",width=100)),
+                                #column(1,img(src="images/arrow.png",width=50)),
+                             column(2,img(src="images/redbox.png",width=100))
+                      
+                    )
+              )
+       
+             )
+  )
+))
