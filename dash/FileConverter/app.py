27141e5f8ce026efa4db97e2e228dced71d83753
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-03-14 21:44:06 -0400

    msconvert functional!

--- dash/FileConverter/app.py
+++ dash/FileConverter/app.py
@@ -1,116 +1,53 @@
-#https://docs.faculty.ai/user-guide/apps/examples/dash_file_upload_download.html
-#also has drag and drop
-
-msconvert = "C:/APPWin32bitBundle/APPServer/bin/pwiz/msconvert.exe"
-
-import base64
-import os
-from urllib.parse import quote as urlquote
-
-from flask import Flask, send_from_directory
+from os.path import join
+from flask import Flask, flash, request
+from werkzeug.utils import secure_filename
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-
-#import raw2mzml
+import os
 import subprocess
+cwd = os.getcwd()
 
-UPLOAD_DIRECTORY = "/project/app_uploaded_files"
+#https://community.plot.ly/t/dash-upload-component-decoding-large-files/8033/10
+#tos:https://community.plot.ly/tos
+#relevant CC license details for community content: https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
+#takeaway: attribution, non-commercial, share-alike
 
-if not os.path.exists(UPLOAD_DIRECTORY):
-    os.makedirs(UPLOAD_DIRECTORY)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+UPLOAD_FOLDER = "/project/app_uploaded_files"
+msconvert = "C:/Program Files/ProteoWizard/ProteoWizard 3.0.19073.85be84641/msconvert.exe"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Normally, Dash creates its own Flask server internally. By creating our own,
-# we can create a route for downloading files directly:
-server = Flask(__name__)
-app = dash.Dash(server=server)
-
-# file.copy(system(paste('msconvert ', input$rawFile$datapath, ' -v --mzML', sep='')), "~/Desktop/folderfolder/x.txt" ,overwrite = TRUE)
-# system(paste('C:/APPWin32bitBundle/APPServer/bin/pwiz/msconvert.exe ', input$rawFile$datapath, ' -o ', my_output_dir), intern = FALSE, ignore.stdout = TRUE, ignore.stderr = TRUE, wait = TRUE, show.output.on.console = TRUE, minimized = FALSE, invisible = FALSE)
-# system('echo Done')
-
-@server.route("/download/<path:path>")
-def download(path):
-    """Serve a file from the upload directory."""
-    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
-
-
-app.layout = html.Div(
-    [
-        html.H1("File Browser"),
-        html.H2("Upload"),
-        dcc.Upload(
-            id="upload-data",
-            #accept=".RAW",
-            children=html.Div(
-                ["Drag and drop or click to select a file to upload."]
-            ),
-            style={
-                "width": "100%",
-                "height": "60px",
-                "lineHeight": "60px",
-                "borderWidth": "1px",
-                "borderStyle": "dashed",
-                "borderRadius": "5px",
-                "textAlign": "center",
-                "margin": "10px",
-            },
-            multiple=False,
-        ),
-        html.H2("File List"),
-        html.Ul(id="file-list"),
-    ],
-    style={"max-width": "500px"},
+app.layout = html.Div( 
+        children=[
+            #html.Iframe(id='iframe-upload',src=f'/upload'),
+            html.Iframe(id='iframe-upload',src=str.format('/upload')),
+            html.Div(id='output')
+                ]
 )
 
+@app.server.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if not os.path.exists(UPLOAD_FOLDER):
+      os.makedirs(UPLOAD_FOLDER)
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        #if file.exists (filename.split('.')[0]+"1." +filename.split('.')[1])
+        while os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+            filename = filename.split('.')[0]+"1." +filename.split('.')[1]
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-def save_file(name, content):
-    """Decode and store a file uploaded with Plotly Dash."""
-    data = content.encode("utf8").split(b";base64,")[1]
-    with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
-        fp.write(base64.decodebytes(data))
-
-
-def uploaded_files():
-    """List the files in the upload directory."""
-    files = []
-    for filename in os.listdir(UPLOAD_DIRECTORY):
-        path = os.path.join(UPLOAD_DIRECTORY, filename)
-        if os.path.isfile(path):
-            files.append(filename)
-    return files
+        cmd = msconvert + " " + os.path.join(app.config['UPLOAD_FOLDER'], filename) + " -o " + UPLOAD_FOLDER
+        subprocess.run(cmd)
 
 
-def file_download_link(filename):
-    """Create a Plotly Dash 'A' element that downloads a file from the app."""
-    location = "/download/{}".format(urlquote(filename))
-    return html.A(filename, href=location)
-
-
-@app.callback(
-    Output("file-list", "children"),
-    [Input("upload-data", "filename"), Input("upload-data", "contents")],
-)
-def update_output(uploaded_filenames, uploaded_file_contents):
-    """Save uploaded files and regenerate the file list."""
-
-    if uploaded_filenames is not None and uploaded_file_contents is not None:
-        # for name, data in zip(uploaded_filenames, uploaded_file_contents):
-        #     save_file(name, data)
-        #cmd = "C:/APPWin32bitBundle/APPServer/bin/pwiz/msconvert.exe"
-        cmd = msconvert + " -help"
-        # os.system(cmd)
-        #os.system(cmd)
-        print(subprocess.check_output(cmd))
-
-    files = uploaded_files()
-    if len(files) == 0:
-        return [html.Li("No files yet!")]
-    else:
-        return [html.Li(file_download_link(filename)) for filename in files]
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True, port=8888)
+    return '''
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+if __name__ == '__main__':
+   app.run_server(debug=True)
---
+++ dash/FileConverter/readme.txt
@@ -0,0 +1,11 @@
+msconvert works!
+
+however, some changes will be needed when run on a machine other than my own
+the two primary changes are that the location of msconvert.exe will vary per machine, and the location which you save to will also vary per preference
+
+another noteworthy detail is that msconver takes a loooong while
+you can ensure that it is working by looing at the debug output in your terminal; msconvert has a number of outputs to signify that it is functioning correctly, and the largest time sink is "writing output file"
+please be patient, and enjoy a functioning converter!
+
+recommendations: after a user has completed upload, they should be forwarded to their unique URL with contents "calculating"
+it should probably also have a message along the lines of "calculations may take some time: please check back in 15 minutes. the webpage is safe to close, but please remember your URL. Your URL code is #### for all future access, and the report, once generated, will be kept on the server at that URL for X days."
