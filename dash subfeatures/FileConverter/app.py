cee07e14731053eaab834582899f857129f53f4c
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-03-24 18:18:59 -0400

    update converter to APP1 version

--- dash subfeatures/FileConverter/app.py
+++ dash subfeatures/FileConverter/app.py
@@ -1,48 +1,53 @@
-from os.path import join
-from flask import Flask, flash, request
-from werkzeug.utils import secure_filename
 import dash
 import dash_html_components as html
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import os
 import subprocess
-cwd = os.getcwd()
+import string
+import random
 
-#proteowizard's msconvert from http://proteowizard.sourceforge.net/download.html
 #https://community.plot.ly/t/dash-upload-component-decoding-large-files/8033/10
-#tos:https://community.plot.ly/tos
-#relevant CC license details for community content: https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
-#takeaway: attribution, non-commercial, share-alike
+#also utilizes proteowizard's msconvert http://proteowizard.sourceforge.net/download.html
+#http://flask.pocoo.org/docs/1.0/patterns/fileuploads/
 
+cwd = os.getcwd()
+UPLOAD_FOLDER = "/project/app_uploaded_files"
+ALLOWED_EXTENSIONS = set(['raw', 'fasta'])
+msconvert = "C:/Program Files/ProteoWizard/ProteoWizard 3.0.19073.85be84641/msconvert.exe"
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-UPLOAD_FOLDER = "/project/app_uploaded_files"
-msconvert = "C:/Program Files/ProteoWizard/ProteoWizard 3.0.19073.85be84641/msconvert.exe"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 app.layout = html.Div( 
         children=[
-            #html.Iframe(id='iframe-upload',src=f'/upload'),
             html.Iframe(id='iframe-upload',src=str.format('/upload')),
             html.Div(id='output')
                 ]
 )
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.server.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if not os.path.exists(UPLOAD_FOLDER):
-      os.makedirs(UPLOAD_FOLDER)
+        os.mkdir(UPLOAD_FOLDER)
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
-        #if file.exists (filename.split('.')[0]+"1." +filename.split('.')[1])
-        while os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
-            filename = filename.split('.')[0]+"1." +filename.split('.')[1]
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        cmd = msconvert + " " + os.path.join(app.config['UPLOAD_FOLDER'], filename) + " -o " + UPLOAD_FOLDER
-        subprocess.run(cmd)
+        if file and allowed_file(file.filename):
+            mspath = UPLOAD_FOLDER
+            file.save(mspath+"/"+filename)
 
+            if filename.rsplit('.', 1)[1].lower() == "raw":
+                cmd = msconvert + " " + mspath +"/" +filename + " -o " + mspath
+                subprocess.run(cmd)
+                
+            print("Done: " + mspath)
+        else:
+            print("Invalid Input")
 
     return '''
     <form method=post enctype=multipart/form-data>
