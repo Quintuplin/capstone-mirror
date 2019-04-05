85f4aa5791af18bc3b24ed620cb057a36bd2fe22
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-04-05 16:49:16 -0400

    multicolor results random script (wip csv read)

--- webapp/modules/APP2.py
+++ webapp/modules/APP2.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 def DIRcheck(ID, basePath):
     if os.path.exists(os.path.join(basePath, str(ID))):
@@ -19,13 +20,15 @@ def RESULTcheck(ID, basePath):
             for name in dirs:
                 print (name)
     #temp dummy filegen so it returns true on second call
-    completeName = os.path.join(basePath, str(ID), "report.csv")         
+    completeName = os.path.join(basePath, str(ID), "report.csv")    
+
     file1 = open(completeName, "w")
-    file1.write("Car;MPG;Cylinders;Displacement;Horsepower;Weight;Acceleration;Model;Origin\n")
-    file1.write("STRING;DOUBLE;INT;DOUBLE;DOUBLE;DOUBLE;DOUBLE;INT;CAT\n")
-    file1.write("Chevrolet Chevelle Malibu;18.0;8;307.0;130.0;3504.;12.0;70;US\n")
-    file1.write("Buick Skylark 320;15.0;8;350.0;165.0;3693.;11.5;70;US\n")
-    file1.write("Plymouth Satellite;18.0;8;318.0;150.0;3436.;11.0;70;US\n")
+    images = ["green", "orange", "red"]
+    subpages = ["sampleimage", "lcimage", "sourceimage", "ms1image", "ms2image"]
+    for subpage in subpages:
+        sample = "{% static 'img/" + str(images[random.randint(0,2)]) +".jpg' %}"
+        print(subpage + ";" + sample + "\n")
+        file1.write(subpage + ";" + sample + "\n")
     file1.close()
 
     return found
