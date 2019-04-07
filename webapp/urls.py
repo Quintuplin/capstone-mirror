5e60ec2a722202acefcb3b2a419a38ffdb4a2be3
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-04-07 12:26:18 -0400

    favicon fix

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,8 +3,6 @@ from django.views.generic import RedirectView
 from django.conf.urls import url
 from . import views
 
-favicon_view = RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)
-
 """
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -20,10 +18,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
-
 urlpatterns = [
-    re_path(r'^favicon\.ico$', favicon_view),
 	path('about/', views.about, name='about'),
     path('about', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
@@ -33,6 +28,5 @@ urlpatterns = [
     path('upload/', views.upload, name='home'),
     path('upload', views.upload, name='home'),
     path('', views.upload, name='home'),
-#     re_path(r'^[\w]', views.results, name="results"), ##everything else gets routed through views.results
     path('<ID>', views.results, name='results'),
 ]
