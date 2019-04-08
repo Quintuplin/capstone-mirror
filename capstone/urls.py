d2cf3bfa3281ef9ba156d743f80e896f97c594c2
Quintuplin <31312410+Quintuplin@users.noreply.github.com>
2019-04-08 12:58:19 -0400

    media linking solved, django documentation-style

--- capstone/urls.py
+++ capstone/urls.py
@@ -3,24 +3,14 @@ edit the webapp.urls instead
 """
 
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("webapp.urls")),
     path("admin/", admin.site.urls),  # Activates the admin interface
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urlpatterns += staticfiles_urlpatterns()
-
-#https://stackoverflow.com/questions/6418072/accessing-media-files-in-django
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-        re_path(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT,
-        }),
-    ]
+urlpatterns += staticfiles_urlpatterns()
