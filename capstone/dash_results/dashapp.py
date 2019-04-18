b4afbff851d5a0ac452de15fe7114ec22f0c5ceb
missMisae <evansme1@g.cofc.edu>
2019-04-17 20:38:47 -0400

    fixing git errors

--- capstone/dash_results/dashapp.py
+++ capstone/dash_results/dashapp.py
@@ -1,21 +1,32 @@
 from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
+
+
+import plotly.plotly as py
 import dash
+import plotly.graph_objs as go
 import os
-
 import dash_table
-
 import numpy as np
 import pandas as pd
-
 from .server import app
 from . import router
+
 # change your directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-#df = pd.read_csv('/home/misae/capstone-2019-nist/capstone/dash_results/temp/que.csv')
+query_file = BASE_DIR + '/que.csv'
+reference_file = BASE_DIR + '/ref.csv'
 
+query_df = pd.read_csv(query_file)
+query_df = query_df.head(50)
+print(query_df)
+
+reference_df = pd.read_csv(reference_file)
+print(reference_df)
 
 
 
@@ -23,8 +34,44 @@ colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
-app.layout = dash_table.DataTable(
-#    id='table',
-#    columns=[{"name": i, "id": i} for i in df.columns],
-#    data=df.to_dict("rows"),
-)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=query_df['Time'],
+                    y=query_df['TIC'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name= 'query'
+                ),
+                go.Scatter(
+                    x = reference_df['Time'],
+                    y = reference_df['TIC'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name='reference'
+                )
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log',
+                       'title': 'Time'},
+                yaxis={'type': 'log',
+                       'title': 'TIC'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
--- capstone/mzml_to_csv.py
+++ capstone/mzml_to_csv.py
@@ -33,24 +33,31 @@ query_file = pyM.run.Reader(query_file)
 
 #reference_file <- openMSfile(paste(REFERENCE, '.mzML', sep=''))
 with open("ref.csv", "w", newline='') as csvfile:
-	writer = csv.writer(csvfile, delimiter=' ',
+	writer = csv.writer(csvfile, delimiter=',',
 						quotechar=' ',quoting=csv.QUOTE_MINIMAL)
+
+	#ref_list = []
+	writer.writerow(['','TIC', 'Time','Sample'])
 
 	n = 0
 	for spectrum in reference_file:
 		ref_tic = spectrum.TIC
 		ref_time = spectrum.scan_time
 		writer.writerow([n,ref_tic, ref_time[0],'HeLa'])
-		n=n+1
+		n+=1
 		#ref_list = ref_list+ref_tic
 
 with open("que.csv", "w", newline='') as csvfile:
-	writer = csv.writer(csvfile, delimiter=' ',
+	writer = csv.writer(csvfile, delimiter=',',
 						quotechar=' ',quoting=csv.QUOTE_MINIMAL)
+
+	writer.writerow(['','TIC', 'Time','Sample'])
+	n = 0
 	for spectrum in query_file:
 		q_tic = spectrum.TIC
 		q_time = spectrum.scan_time
-		writer.writerow([q_tic, q_time[0],'HeLa'])
+		writer.writerow([n, q_tic, q_time[0],'HeLa'])
+		n+=1
 		#writer.write("\n")
 		#print("reference TIC: ", ref_tic)
 
